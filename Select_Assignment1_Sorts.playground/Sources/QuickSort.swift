f845422b2a3045b3017cc60e1aae99ac96c24008
John Whisker <NGUYEN.DK0952@sinhvien.hoasen.edu.vn>
2016-06-26 06:05:25 +0700

    update quickSort

--- Select_Assignment1_Sorts.playground/Sources/QuickSort.swift
+++ Select_Assignment1_Sorts.playground/Sources/QuickSort.swift
@@ -1,7 +1,14 @@
 import Foundation
 
-public func quickSort(list: [Int]) -> [Int] {
-   
-    return list
+public func quickSort<T: Comparable>(list: [T])->[T]{
+    guard list.count>1 else {return list}
+    let privot = list[list.count/2]
+    let less = list.filter {$0 < privot}
+    let equal = list.filter {$0 == privot}
+    let greater = list.filter {$0 > privot}
+    
+    return quickSort(less) + equal + quickSort(greater);
+    
 }
+
 
