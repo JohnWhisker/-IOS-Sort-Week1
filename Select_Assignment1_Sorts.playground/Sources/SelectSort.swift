4c22d06a4436d7c18fac67257dd88992ae3d2bba
John Whisker <NGUYEN.DK0952@sinhvien.hoasen.edu.vn>
2016-06-26 06:07:28 +0700

    update selectSort

--- Select_Assignment1_Sorts.playground/Sources/SelectSort.swift
+++ Select_Assignment1_Sorts.playground/Sources/SelectSort.swift
@@ -1,7 +1,19 @@
 import Foundation
 
-public func selectSort(list: [Int]) -> [Int] {
-    
-    return list
+public func selectSort(list: [Int]) ->[Int]{
+    guard list.count > 1 else {return list}
+    var a = list
+    for x in 0..<a.count-1{
+        var lowest = x
+        for y in x + 1 ..< a.count {
+            if a[y] < a[lowest]{
+                lowest = y;
+            }
+        }
+        
+        if x != lowest{
+            a.swap(x, index2: lowest)
+        }
+    }
+    return a;
 }
-
